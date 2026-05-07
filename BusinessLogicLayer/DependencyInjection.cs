ba06cf7eaf8288cff6384891af2a483ee85e965a
Arvis Iljins <arvis.iljins@elkogroup.com>
2026-05-07 20:23:23 +0300

    Add initial solution and project files for OrdersMicroservice.API
    - Created a new solution file for the eCommerce Orders Service.
    - Added project references for BusinessLogicLayer, DataAccessLayer, and OrdersMicroservice.API.
    - Included project.nuget.cache to manage NuGet package dependencies.
    - Noted a high severity vulnerability in AutoMapper package version 13.0.1.

---
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.OrderMicroservice.BusinessLogicLayer;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusinessLogicLayer(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        //TO DO: Add business logic layer services into the IoC container
+
+        return services;
+    }
+}
